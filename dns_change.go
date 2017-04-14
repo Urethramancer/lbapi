9bfae65aa085334591d13fe18ad6be34b3005be3
Ronny Bangsund <github@neuralshortcircuit.com>
2017-04-15 00:45:22 +0200

    API call for A/AAAA record change added.

--- dns_change.go
+++ dns_change.go
@@ -1,5 +1,50 @@
 package lbapi
 
-func (c *Client) ChangeDNSRecord(rec *DNSRecord) error {
+import (
+	"errors"
+	"fmt"
+	"net/url"
+)
+
+func (c *Client) ChangeARecord(domain, oldip, newip, host string, ttl int64, six bool) error {
+	if six {
+		return c.changeRecord("api/dns/manage/update-ipv6-record.json", domain, oldip, newip, host, ttl)
+	}
+	return c.changeRecord("api/dns/manage/update-ipv4-record.json", domain, oldip, newip, host, ttl)
+}
+
+func (c *Client) changeRecord(call, domain, oldip, newip, host string, ttl int64) error {
+	var err error
+	u, err := url.Parse(c.URL)
+	if err != nil {
+		return err
+	}
+
+	u.Path = call
+	q := u.Query()
+	q.Set("auth-userid", c.ID)
+	q.Set("api-key", c.Key)
+	q.Set("domain-name", domain)
+	q.Set("current-value", oldip)
+	q.Set("new-value", newip)
+	if host != "" {
+		q.Set("host", host)
+	}
+	if ttl == 0 || ttl < 7200 {
+		ttl = 7200
+	}
+	q.Set("ttl", fmt.Sprintf("%d", ttl))
+	u.RawQuery = q.Encode()
+
+	res, err := c.postResponse(u.String())
+	if err != nil {
+		return err
+	}
+
+	list := *res
+	if list["status"] == "ERROR" {
+		return errors.New((fmt.Sprintf("%v", list["message"])))
+	}
+
 	return nil
 }
