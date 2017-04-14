dad42010adf18cd00f1448df735f3eb2d887fd44
Ronny Bangsund <github@neuralshortcircuit.com>
2017-04-15 01:38:17 +0200

    lbmgr can now add, modify and delete 7 DNS record types.

--- cmd/lbmgr/cmd_dns_change.go
+++ cmd/lbmgr/cmd_dns_change.go
@@ -45,50 +45,83 @@ func (cmd *DNSChangeAAAACmd) Execute(args []string) error {
 
 // DNSChangeCNAMECmd arguments.
 type DNSChangeCNAMECmd struct {
-	Args DNSAddArgs `positional-args:"true"`
+	Args DNSChangeArgs `positional-args:"true"`
 }
 
 // DNSChangeCNAMECmd modifies a CNAME record.
 func (cmd *DNSChangeCNAMECmd) Execute(args []string) error {
+	err := client.ChangeCNAME(cmd.Args.Domain, cmd.Args.Old, cmd.Args.New, cmd.Args.Host, cmd.Args.TTL)
+	if err != nil {
+		return err
+	}
+
+	pr("Record added.")
 	return nil
 }
 
 // DNSChangeMXCmd arguments.
 type DNSChangeMXCmd struct {
-	Args DNSAddArgs `positional-args:"true"`
+	Args DNSChangeArgsPri `positional-args:"true"`
 }
 
 // DNSChangeMXCmd modifies an MX record.
 func (cmd *DNSChangeMXCmd) Execute(args []string) error {
+	err := client.ChangeMX(cmd.Args.Domain, cmd.Args.Old, cmd.Args.New, cmd.Args.Host, cmd.Args.TTL, cmd.Args.Priority)
+	if err != nil {
+		return err
+	}
+
+	pr("Record added.")
 	return nil
 }
 
 // DNSChangeNSCmd arguments.
 type DNSChangeNSCmd struct {
-	Args DNSAddArgs `positional-args:"true"`
+	Args DNSChangeArgs `positional-args:"true"`
 }
 
 // DNSChangeNSCmd modifies an NS record.
 func (cmd *DNSChangeNSCmd) Execute(args []string) error {
+	err := client.ChangeNS(cmd.Args.Domain, cmd.Args.Old, cmd.Args.New, cmd.Args.Host, cmd.Args.TTL)
+	if err != nil {
+		return err
+	}
+
+	pr("Record added.")
 	return nil
 }
 
 // DNSChangeTXTCmd arguments.
 type DNSChangeTXTCmd struct {
-	Args DNSAddArgs `positional-args:"true"`
+	Args DNSChangeArgs `positional-args:"true"`
 }
 
 // DNSChangeTXTCmd modifies a TXT record.
 func (cmd *DNSChangeTXTCmd) Execute(args []string) error {
+	err := client.ChangeTXT(cmd.Args.Domain, cmd.Args.Old, cmd.Args.New, cmd.Args.Host, cmd.Args.TTL)
+	if err != nil {
+		return err
+	}
+
+	pr("Record added.")
 	return nil
 }
 
 // DNSChangeSRVCmd arguments.
 type DNSChangeSRVCmd struct {
-	Args DNSAddArgs `positional-args:"true"`
+	Args struct {
+		Domain   string `required:"true" positional-arg-name:"DOMAIN" description:"Domain name."`
+		Old      string `required:"true" positional-arg-name:"OLDIP" description:"IP address to change from."`
+		New      string `required:"true" positional-arg-name:"NEWIP" description:"New IP address."`
+		Host     string `required:"true" positional-arg-name:"HOST" description:"A fully qualified service name in the form '_<service name>._<protocol>.<domain.tld>'."`
+		TTL      int64  `positional-arg-name:"TTL" description:"Time to live (seconds)."`
+		Priority uint   `positional-arg-name:"PRIORITY" description:"Priority of record. Default is 0 (most important)."`
+		Port     uint   `positional-arg-name:"PORT" description:"Port number of the service."`
+		Weight   uint   `positional-arg-name:"WEIGHT" description:"Relative weight for records with the same priority."`
+	} `positional-args:"true"`
 }
 
 // DNSChangeSRVCmd modifies a SRV record.
 func (cmd *DNSChangeSRVCmd) Execute(args []string) error {
-	return nil
+	return client.ChangeSRV(cmd.Args.Domain, cmd.Args.Old, cmd.Args.New, cmd.Args.Host, cmd.Args.TTL, cmd.Args.Priority, cmd.Args.Port, cmd.Args.Weight)
 }
--- dns_change.go
+++ dns_change.go
@@ -6,11 +6,28 @@ import (
 	"net/url"
 )
 
+// ChangeARecord modifies A or AAAA records.
 func (c *Client) ChangeARecord(domain, oldip, newip, host string, ttl int64, six bool) error {
 	if six {
 		return c.changeRecord("api/dns/manage/update-ipv6-record.json", domain, oldip, newip, host, ttl)
 	}
 	return c.changeRecord("api/dns/manage/update-ipv4-record.json", domain, oldip, newip, host, ttl)
+}
+
+func (c *Client) ChangeCNAME(domain, oldip, newip, host string, ttl int64) error {
+	return c.changeRecord("api/dns/manage/update-cname-record.json", domain, oldip, newip, host, ttl)
+}
+
+func (c *Client) ChangeMX(domain, oldip, newip, host string, ttl int64, priority uint) error {
+	return c.changeRecordPri("api/dns/manage/update-mx-record.json", domain, oldip, newip, host, ttl, priority)
+}
+
+func (c *Client) ChangeNS(domain, oldip, newip, host string, ttl int64) error {
+	return c.changeRecord("api/dns/manage/update-ns-record.json", domain, oldip, newip, host, ttl)
+}
+
+func (c *Client) ChangeTXT(domain, oldip, newip, host string, ttl int64) error {
+	return c.changeRecord("api/dns/manage/update-txt-record.json", domain, oldip, newip, host, ttl)
 }
 
 func (c *Client) changeRecord(call, domain, oldip, newip, host string, ttl int64) error {
@@ -34,6 +51,89 @@ func (c *Client) changeRecord(call, domain, oldip, newip, host string, ttl int64
 		ttl = 7200
 	}
 	q.Set("ttl", fmt.Sprintf("%d", ttl))
+	u.RawQuery = q.Encode()
+
+	res, err := c.postResponse(u.String())
+	if err != nil {
+		return err
+	}
+
+	list := *res
+	if list["status"] == "ERROR" {
+		return errors.New((fmt.Sprintf("%v", list["message"])))
+	}
+
+	return nil
+}
+
+func (c *Client) changeRecordPri(call, domain, oldip, newip, host string, ttl int64, priority uint) error {
+	var err error
+	u, err := url.Parse(c.URL)
+	if err != nil {
+		return err
+	}
+
+	u.Path = call
+	q := u.Query()
+	q.Set("auth-userid", c.ID)
+	q.Set("api-key", c.Key)
+	q.Set("domain-name", domain)
+	q.Set("current-value", oldip)
+	q.Set("new-value", newip)
+	if host != "" {
+		q.Set("host", host)
+	}
+	if ttl == 0 || ttl < 7200 {
+		ttl = 7200
+	}
+	q.Set("ttl", fmt.Sprintf("%d", ttl))
+	q.Set("priority", fmt.Sprintf("%d", priority))
+	u.RawQuery = q.Encode()
+
+	res, err := c.postResponse(u.String())
+	if err != nil {
+		return err
+	}
+
+	list := *res
+	if list["status"] == "ERROR" {
+		return errors.New((fmt.Sprintf("%v", list["message"])))
+	}
+
+	return nil
+}
+
+// ChangeSRV modifies a SRV record.
+func (c *Client) ChangeSRV(domain, oldval, newval, host string, ttl int64, priority, port, weight uint) error {
+	var err error
+	u, err := url.Parse(c.URL)
+	if err != nil {
+		return err
+	}
+
+	u.Path = "api/dns/manage/add-srv-record.json"
+	q := u.Query()
+	q.Set("auth-userid", c.ID)
+	q.Set("api-key", c.Key)
+	q.Set("domain-name", domain)
+	q.Set("current-value", oldval)
+	q.Set("new-value", newval)
+	q.Set("host", host)
+	if ttl == 0 || ttl < 7200 {
+		ttl = 7200
+	}
+	if ttl > 0 {
+		q.Set("ttl", fmt.Sprintf("%d", ttl))
+	}
+	if priority > 0 {
+		q.Set("priority", fmt.Sprintf("%d", priority))
+	}
+	if port > 0 {
+		q.Set("port", fmt.Sprintf("%d", port))
+	}
+	if weight > 0 {
+		q.Set("weight", fmt.Sprintf("%d", weight))
+	}
 	u.RawQuery = q.Encode()
 
 	res, err := c.postResponse(u.String())
