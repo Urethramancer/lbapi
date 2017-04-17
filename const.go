e6312c24a948ed22877bfa72bcccf75de431cb60
Ronny Bangsund <github@neuralshortcircuit.com>
2017-04-17 18:17:08 +0200

    Starting to add constants for API base strings.

--- const.go
+++ const.go
@@ -5,6 +5,10 @@ const (
 	APIURL = "https://httpapi.com/api/"
 	// APITESTURL is the LogicBoxes API URL used for test accounts.
 	APITESTURL = "https://test.httpapi.com/api/"
+
+	API_CUSTOMERS_DETAILS_BY_ID = "api/customers/details-by-id.json"
+	// API_DOMAINS_SEARCH is the source of all domain order information.
+	API_DOMAINS_SEARCH = "api/domains/search.json"
 )
 
 type maplist map[string]interface{}
