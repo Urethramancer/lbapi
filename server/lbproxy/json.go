97e0df74ba82cec8629aa8c0de334d95a9a7c02f
Ronny Bangsund <github@neuralshortcircuit.com>
2017-04-23 01:06:28 +0200

    Removed leftover Request structure.

--- server/lbproxy/json.go
+++ server/lbproxy/json.go
@@ -5,30 +5,6 @@ import (
 	"io"
 )
 
-// Request is a generic all-purpose JSON request structure.
-type Request struct {
-	// Token is required for all requests to manipulate data, or read privileged information.
-	Token string `json:"token,omitempty"`
-	// Username is used for authentication requests only.
-	Username string `json:"username,omitempty"`
-	// Password is used for authentication requests only.
-	Password string `json:"password,omitempty"`
-	// ID is used to retrieve notes and lists.
-	ID uint64 `json:"id,omitempty"`
-	// Note is used when adding notes.
-	// For changing public/private status
-	Public bool   `json:"public,omitempty"`
-	Note   string `json:"note,omitempty"`
-	// List is used when adding lists.
-	List string `json:"list,omitempty"`
-	// ListItems are used when creating lists or appending to them.
-	ListItems []string `json:"listitems,omitempty"`
-	// Start of a range to fetch.
-	Start int `json:"start,omitempty"`
-	// Count is the number of items to fetch.
-	Count int `json:"count,omitempty"`
-}
-
 // Result is returned from any JSON request.
 type Result struct {
 	Status     string `json:"status,omitempty"`
